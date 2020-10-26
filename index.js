aa12a41ff09d8912b024293a53a850665b9ceaea
Catalin Pit <catalinpit@gmail.com>
2020-10-26 09:51:33 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+const server = app.listen(3000, () => {
+    console.log(`The application started on port ${server.address().port}`);
+});
